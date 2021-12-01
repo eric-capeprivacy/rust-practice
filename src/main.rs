fe231f834a9c5ee7973e8a53555ba50bebca2700
Eric Zhang <eric@capeprivacy.com>
2021-12-01 03:07:06 -0500

    added macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 macro_rules! add_as{
     (
   // repeated block
@@ -15,10 +17,27 @@ macro_rules! add_as{
      }
     }
 }
+#[derive(Debug)]
+struct Developer {
+    pub age: i32,
+}
 
+macro_rules! debug_display {
+    ($t:ident) => {
+        impl std::fmt::Display for $t {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "{:?}", self)
+            }
+        }
+    }
+}
+
+debug_display!(Developer);
 // add a macro that identfies a trait by returning a function pointer
 
 
 fn main(){
+    let dev : Developer = Developer{age: 12};
     println!("{}",add_as!(1,2,3,4)); // => println!("{}",{0+1+2+3+4})
+    println!("{}", dev);
 }
